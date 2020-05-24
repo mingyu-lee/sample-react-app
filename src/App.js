553b4551f13c5d339b2adb460eec8ac84d625782
Mingyu Lee <leemingyu05@gmail.com>
2020-05-24 22:27:32 +0900

    #3.0 Class Components and State

--- src/App.js
+++ src/App.js
@@ -1,56 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const todos = [
-    {
-        id: 1,
-        name: '자바 공부하기',
-        imageUrl: 'https://www.oracle.com/a/ocom/img/cb71-java-logo.png'
-    },
-    {
-        id: 2,
-        name: '리액트 공부하기',
-        imageUrl: 'https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png'
-    },
-    {
-        id: 3,
-        name: '도커 공부하기',
-        imageUrl: 'https://miro.medium.com/proxy/1*9hGvYE5jegHm1r_97gH-jQ.png'
-    },
-    {
-        id: 4,
-        name: '운동하기',
-        imageUrl: 'https://thumbs.gfycat.com/FittingIdenticalAtlanticspadefish-mobile.jpg'
+class App extends React.Component {
+    state = {
+        count: 0
+    };
+    increase = () => {
+        console.log("increase");
+    };
+    decrease = () => {
+        console.log("decrease");
+    };
+    render() {
+        return <div>
+            <h3>Count: {this.state.count}</h3>
+            <button onClick={this.increase}>Increase</button>
+            <button onClick={this.decrease}>Decrease</button>
+        </div>
+        ;
     }
-]
-
-Todos.propTypes = {
-    name: PropTypes.string.isRequired,
-    // will be error
-    // name: PropTypes.number.isRequired,
-    imageUrl: PropTypes.string
-}
-
-function Todos({name, imageUrl}) {
-    return (
-        <li>
-            <input type="checkbox" name="todo"/>
-            <label style={{marginLeft: 2}}>{name}</label><br/>
-            <img src={imageUrl} style={{width: 100}} alt={name}/>
-        </li>
-    );
-}
-
-function App() {
-  return (
-    <div>
-      <form>
-        <ul>
-            {todos.map(todo => <Todos key={todo.id} name={todo.name} imageUrl={todo.imageUrl}/>)}
-        </ul>
-      </form>
-    </div>
-  );
 }
 
 export default App;
