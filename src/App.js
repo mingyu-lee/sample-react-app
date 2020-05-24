b34abdb0ddb83094b2054b63c22c7ee8b3b34583
Mingyu Lee <leemingyu05@gmail.com>
2020-05-24 21:06:44 +0900

    #2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
-function Todos({todo}) {
+const todos = [
+    {
+        name: '자바 공부하기',
+        imageUrl: 'https://www.oracle.com/a/ocom/img/cb71-java-logo.png'
+    },
+    {
+        name: '리액트 공부하기',
+        imageUrl: 'https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png'
+    },
+    {
+        name: '도커 공부하기',
+        imageUrl: 'https://miro.medium.com/proxy/1*9hGvYE5jegHm1r_97gH-jQ.png'
+    },
+    {
+        name: '운동하기',
+        imageUrl: 'https://thumbs.gfycat.com/FittingIdenticalAtlanticspadefish-mobile.jpg'
+    }
+]
+
+
+function Todos({todo, imageUrl}) {
     return (
         <li>
             <input type="checkbox" name="todo"/>
-            <label style={{marginLeft: 2}}>{todo}</label>
+            <label style={{marginLeft: 2}}>{todo}</label><br/>
+            <img src={imageUrl} style={{width: 100}}/>
         </li>
     );
 }
@@ -12,13 +33,9 @@ function Todos({todo}) {
 function App() {
   return (
     <div>
-      Hello Sample React App!!!<br/>
       <form>
         <ul>
-            <Todos todo="리액트 공부하기" />
-            <Todos todo="자바 공부하기" />
-            <Todos todo="도커 공부하기" />
-            <Todos todo="운동하기" />
+            {todos.map(todo => <Todos todo={todo.name} imageUrl={todo.imageUrl}/>)}
         </ul>
       </form>
     </div>
