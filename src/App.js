5decc817350c0d5a2f7d77ad002471d0a8fe979b
Mingyu Lee <leemingyu05@gmail.com>
2020-05-24 21:52:07 +0900

    #2.3 .map Recap

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React from 'react';
 
 const todos = [
     {
+        id: 1,
         name: '자바 공부하기',
         imageUrl: 'https://www.oracle.com/a/ocom/img/cb71-java-logo.png'
     },
     {
+        id: 2,
         name: '리액트 공부하기',
         imageUrl: 'https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png'
     },
     {
+        id: 3,
         name: '도커 공부하기',
         imageUrl: 'https://miro.medium.com/proxy/1*9hGvYE5jegHm1r_97gH-jQ.png'
     },
     {
+        id: 4,
         name: '운동하기',
         imageUrl: 'https://thumbs.gfycat.com/FittingIdenticalAtlanticspadefish-mobile.jpg'
     }
@@ -25,7 +29,7 @@ function Todos({todo, imageUrl}) {
         <li>
             <input type="checkbox" name="todo"/>
             <label style={{marginLeft: 2}}>{todo}</label><br/>
-            <img src={imageUrl} style={{width: 100}}/>
+            <img src={imageUrl} style={{width: 100}} alt={todo}/>
         </li>
     );
 }
@@ -35,7 +39,7 @@ function App() {
     <div>
       <form>
         <ul>
-            {todos.map(todo => <Todos todo={todo.name} imageUrl={todo.imageUrl}/>)}
+            {todos.map(todo => <Todos key={todo.id} todo={todo.name} imageUrl={todo.imageUrl}/>)}
         </ul>
       </form>
     </div>
