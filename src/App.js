0a21feff6c07bbdaca2787719a9e59114cffb69e
Mingyu Lee <leemingyu05@gmail.com>
2020-05-24 15:01:45 +0900

    #2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import Component from './Component'
+
+function Todos({todo}) {
+    return (
+        <li>
+            <input type="checkbox" name="todo"/>
+            <label style={{marginLeft: 2}}>{todo}</label>
+        </li>
+    );
+}
 
 function App() {
   return (
     <div>
-      Hello Sample React App!!!
-        <Component />
+      Hello Sample React App!!!<br/>
+      <form>
+        <ul>
+            <Todos todo="리액트 공부하기" />
+            <Todos todo="자바 공부하기" />
+            <Todos todo="도커 공부하기" />
+            <Todos todo="운동하기" />
+        </ul>
+      </form>
     </div>
   );
 }
