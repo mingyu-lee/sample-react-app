08a0fe3d3ac1ee01625ece965f589e7eef64bbc3
Mingyu Lee <leemingyu05@gmail.com>
2020-05-31 18:21:05 +0900

    Updates

---
+++ precache-manifest.6f2f3f570bc81290acecbee8cac0481b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0284030812e05bd1de403b49e9007701",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c8bb2df76f7632f7679",
+    "url": "/static/css/main.1c0d20dd.chunk.css"
+  },
+  {
+    "revision": "549b76c690c70c5b02fa",
+    "url": "/static/js/2.a959b1a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a959b1a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c8bb2df76f7632f7679",
+    "url": "/static/js/main.b1d9cb1e.chunk.js"
+  },
+  {
+    "revision": "c524a5c5e924991fcb2d",
+    "url": "/static/js/runtime-main.0a0e86c1.js"
+  }
+]);
